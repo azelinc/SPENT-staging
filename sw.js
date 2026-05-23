19a7d67b0e33dcdd104fb06449f5b52e4222a977
Azly <azelinc@gmail.com>
2026-05-23 08:38:30 +0000

    v1.6: rename assets sp.css/sp.js for cache bust; add version indicator; fix API key

--- sw.js
+++ sw.js
@@ -1,27 +1,18 @@
-const CACHE = 'spent-v6';
+const CACHE = 'spent-v7';
 const FILES = [
   '/SPENT/',
   '/SPENT/index.html',
-  '/SPENT/app.css',
-  '/SPENT/app.js',
+  '/SPENT/sp.css',
+  '/SPENT/sp.js',
   '/SPENT/manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/SPENT/')))
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/SPENT/'))));
 });
