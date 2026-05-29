7b3b94f56b094c7630e28ccce4814a481c9f3082
Azly <azlyubs@gmail.com>
2026-05-29 09:43:50 +0000

    chore: bump to v2.5.0

--- sp7.js
+++ sp7.js
@@ -17,7 +17,7 @@ firebase.initializeApp(FIREBASE_CONFIG);
 const auth = firebase.auth();
 const db = firebase.database();
 
-const APP_VER = 'v2.4.9';
+const APP_VER = 'v2.5.0';
 $('global-version').textContent = APP_VER;
 
 /* ─── CONSTANTS ─── */
