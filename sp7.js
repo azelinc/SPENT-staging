53bc9d69576f174db8ae080e44384c1b66b584d8
Azly <azel.inc@gmail.com>
2026-06-01 01:49:18 +0000

    v2.8.1

--- sp7.js
+++ sp7.js
@@ -17,7 +17,7 @@ firebase.initializeApp(FIREBASE_CONFIG);
 const auth = firebase.auth();
 const db = firebase.database();
 
-const APP_VER = 'v2.8.0';
+const APP_VER = 'v2.8.1';
 $('global-version').textContent = APP_VER;
 
 /* ─── CONSTANTS ─── */
